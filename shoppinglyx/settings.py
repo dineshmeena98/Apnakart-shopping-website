e0d0164bf0411f09c6a5c07e370f6de6ef8e638a
Dinesh Kumar Meena <dineshmeena982838@gmail.com>
2022-08-30 00:03:54 +0530

    commit 1 test

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+*_glfb+61yfwpfb7a!!k_gkr*^+j_$z%$x3s9g#tbti3@k662
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','apnakart1.herokuapp.com/']
 
 
 # Application definition
